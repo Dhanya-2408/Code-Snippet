ac984d01a6bd879db4b616337dcc5a27fd411203
DhanyaInfovenz <dhanya@infovenz.com>
2024-12-28 20:36:13 +1000

    Snippet Show Page

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/src/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const { id } = await props.params;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return "Edit Snippet";
+}
